fa614d4e619a96c3eca0c4bb83a202b1247c02e9
Tom Ward <tomward@fmail.co.uk>
2021-01-09 17:35:36 +0000

    Change band size to 2 rows

--- dither.py
+++ dither.py
@@ -3,12 +3,12 @@ import random
 
 image_height = 32
 image_width = 64
-band_size = 4
+band_size = 2
 new_img = Image.new('RGB', (image_width, image_height), '#b10019')
 pencil = ImageDraw.Draw(new_img)
 for row in range(image_height):
     band = row//band_size
-    ratio = (2+(band*2))/16
+    ratio = (1+(band))/16
     for col in range(image_width):
         if random.random() > ratio:
             print('.', end="")
