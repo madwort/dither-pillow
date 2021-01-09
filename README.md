8d855cd6f4e7a8f951be78ac609916b03e7f59ca
Tom Ward <tomward@fmail.co.uk>
2021-01-09 17:34:55 +0000

    Initial check-in

---
+++ README.md
@@ -0,0 +1,12 @@
+# dither
+
+Simple thing to generate some random dithering patterns for knitting a jumper.
+
+## How to use
+
+```bash
+python3 -m virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python dither.py
+```
---
+++ dither.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw
+import random
+
+image_height = 32
+image_width = 64
+band_size = 4
+new_img = Image.new('RGB', (image_width, image_height), '#b10019')
+pencil = ImageDraw.Draw(new_img)
+for row in range(image_height):
+    band = row//band_size
+    ratio = (2+(band*2))/16
+    for col in range(image_width):
+        if random.random() > ratio:
+            print('.', end="")
+            # pencil.rectangle((row, col, 1, 1), fill ='#310074')
+        else:
+            print(' ', end="")
+    print("  ", "row:", row, "band:", band, "ratio:", ratio)
+
+# new_img.show()
